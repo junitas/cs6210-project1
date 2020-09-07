eec1f5632ea3eaffdfab8b5c73742593320fc5e3
junitas <junitas@riseup.net>
2020-09-07 12:39:57 -0700

    Get vCPU info from a guest

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <libvirt/libvirt.h>
 
 int main() {
@@ -15,6 +16,23 @@ int main() {
     **/
    int numOfDomains = virConnectNumOfDomains(hypervisor);
    printf("\nNumber of domains: %i", numOfDomains);
+   printf("\nGetting pointers to all domains...");
+   virDomainPtr** allDomains = malloc(numOfDomains * sizeof(virDomainPtr));
+   int res = virConnectListAllDomains(hypervisor, allDomains, 0);
+
+   virVcpuInfoPtr vCpuInfo = malloc(sizeof(virVcpuInfo));
+   unsigned char ** cpuMap = malloc(sizeof(char*));
+   int numOfHostCpus = virNodeGetCPUMap(hypervisor, cpuMap, NULL, 0);
+   printf("\nFound %i real CPUs on host.\n", numOfHostCpus);
+
+
+   int res2 = virDomainGetVcpus(**allDomains, vCpuInfo, 3, *cpuMap, sizeof(*cpuMap));
+   printf("\nReturn value from virDomainGetVcpus: %i\n", res2);
+
+   printf("\nvCPU Number: %i, vCpu State: %i, vCpu CPU Time (ns): %llu, pCpu Number: %i\n", vCpuInfo->number, vCpuInfo->state, vCpuInfo->cpuTime, vCpuInfo->cpu);
+
+
+   
    
    return 0;
 }
