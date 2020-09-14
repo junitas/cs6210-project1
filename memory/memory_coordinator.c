f10acf0013614426c21f8ad7bfa8a535172cdc33
junitas <junitas@riseup.net>
2020-09-14 16:23:50 -0700

    Fix bug where I wasn't getting memory stats

--- memory/memory_coordinator.c
+++ memory/memory_coordinator.c
@@ -22,23 +22,20 @@ int balanceMemory() {
 }
 
 int printMemoryStats() {
-    virDomainMemoryStatPtr stats = calloc(VIR_DOMAIN_MEMORY_STAT_NR, sizeof(virDomainMemoryStatStruct));
-
 	for(int i = 0; i < numOfDomains; i++) {
+		virDomainMemoryStatPtr stats = calloc(VIR_DOMAIN_MEMORY_STAT_NR, sizeof(virDomainMemoryStatStruct));
 		printf("Domain: %s\n", virDomainGetName(allDomains[i]));
 		int maxMem = virDomainGetMaxMemory(allDomains[i]);
 		printf("Max physical memory allocated to domain: %uKiB = %uKB\n", maxMem, maxMem);
 		int numRet = virDomainMemoryStats(allDomains[i], stats, VIR_DOMAIN_MEMORY_STAT_NR, 0);
-		
+		printf("Got %i stats about domain.\n", numRet);
 		unsigned long long unused = DEFAULT_DEBUG_VALUE;
 		unsigned long long available = DEFAULT_DEBUG_VALUE;
 		unsigned long long balloonSize = DEFAULT_DEBUG_VALUE;
 		unsigned long long usable = DEFAULT_DEBUG_VALUE;
-
-        // SHOULD I BE CALLING VIRDOMAINGETINFO HERE?
        
 		for(int j = 0; j < numRet; j++) {
-			virDomainMemoryStatStruct s = stats[i];
+			virDomainMemoryStatStruct s = stats[j];
 			if (s.tag == VIR_DOMAIN_MEMORY_STAT_UNUSED) unused = s.val;
 			if (s.tag == VIR_DOMAIN_MEMORY_STAT_AVAILABLE) available = s.val;
 			if (s.tag == VIR_DOMAIN_MEMORY_STAT_ACTUAL_BALLOON) balloonSize = s.val;
@@ -53,16 +50,19 @@ int printMemoryStats() {
 
 
 int main(int argc, char * argv) {
+	  while(1) {
       hypervisor = virConnectOpen(NULL);
 
       populateAllDomainsArray();
       for(int i = 0; i < numOfDomains; i++) virDomainSetMemoryStatsPeriod(allDomains[i], 1, 0);
-      sleep(5);
+      sleep(2);
       printMemoryStats();
       // coordinate memory here.
 
       freeAllDomainPointers();
       virConnectClose(hypervisor);
+      sleep(5);	
+  }
 }
 
 
@@ -90,7 +90,7 @@ giving all guests their fair share of memory.
 
 
 VMWare uses the idle tax ballooning approach, which
-I will use as well.
+I will use as well. or will i? do i have that info?
 
 
 Mem.IdleTax – default: 75, range: 0 to 99, specifies the percent of idle memory
