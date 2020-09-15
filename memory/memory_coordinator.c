7c046cba71d5e866154c0267a6df3bab151c12af
junitas <junitas@riseup.net>
2020-09-14 17:16:54 -0700

    more work on memory stuff

--- memory/memory_coordinator.c
+++ memory/memory_coordinator.c
@@ -11,6 +11,7 @@
 
 #define DEFAULT_DEBUG_VALUE 69
 
+double MEMORY_USAGE_RED_ALERT = 0.75;
 
 int numOfDomains;
 virDomainPtr* allDomains;
@@ -18,13 +19,51 @@ virConnectPtr hypervisor;
 
 
 int balanceMemory() {
+	int domainToGiveMemory = -1;
+	double highestMemoryUsageRatio = 0.0;
+	for(int i = 0; i < numOfDomains; i++) {
+		virDomainMemoryStatPtr stats = calloc(VIR_DOMAIN_MEMORY_STAT_NR, sizeof(virDomainMemoryStatStruct));
+		int maxMem = virDomainGetMaxMemory(allDomains[i]);
+		int numRet = virDomainMemoryStats(allDomains[i], stats, VIR_DOMAIN_MEMORY_STAT_NR, 0);
+		unsigned long long unused = DEFAULT_DEBUG_VALUE;
+		unsigned long long available = DEFAULT_DEBUG_VALUE;
+		unsigned long long balloonSize = DEFAULT_DEBUG_VALUE;
+		unsigned long long usable = DEFAULT_DEBUG_VALUE;
+       
+		for(int j = 0; j < numRet; j++) {
+			virDomainMemoryStatStruct s = stats[j];
+			if (s.tag == VIR_DOMAIN_MEMORY_STAT_UNUSED) unused = s.val;
+			if (s.tag == VIR_DOMAIN_MEMORY_STAT_AVAILABLE) available = s.val;
+			if (s.tag == VIR_DOMAIN_MEMORY_STAT_ACTUAL_BALLOON) balloonSize = s.val;
+			if (s.tag == VIR_DOMAIN_MEMORY_STAT_USABLE) usable = s.val;
+		}
+
+		// If the ratio is above 0.75, and this VM doesn't
+		// already have too much memory allocated, and it 
+		// has the worst memory ratio
+        double r = (float)unused/(float)(available);
+		if (r >= MEMORY_USAGE_RED_ALERT && r >= highestMemoryUsageRatio) {
+             highestMemoryUsageRatio = r;
+             domainToGiveMemory = i;
+		}
+
+		// What if a VM is using less than 0.75 of its memory?
+		// I guess leave it alone.
+	}
+	if (domainToGiveMemory == -1) {
+		printf("No domain has above 0.75 memory usage.\n");
+		return 0;
+	}
+	printf("\nDomain %s to be given more memory.\n\n", virDomainGetName(allDomains[domainToGiveMemory]));
+	return 0;
+
 
 }
 
 int printMemoryStats() {
 	for(int i = 0; i < numOfDomains; i++) {
 		virDomainMemoryStatPtr stats = calloc(VIR_DOMAIN_MEMORY_STAT_NR, sizeof(virDomainMemoryStatStruct));
-		printf("Domain: %s\n", virDomainGetName(allDomains[i]));
+		printf("\nDomain: %s\n", virDomainGetName(allDomains[i]));
 		int maxMem = virDomainGetMaxMemory(allDomains[i]);
 		printf("Max physical memory allocated to domain: %uKiB = %uKB\n", maxMem, maxMem);
 		int numRet = virDomainMemoryStats(allDomains[i], stats, VIR_DOMAIN_MEMORY_STAT_NR, 0);
@@ -45,6 +84,7 @@ int printMemoryStats() {
 		printf("Memory usable by domain:      %llukB\n", available);
 		printf("Current balloon size:         %llukB\n", balloonSize);
 		printf("How much the balloon can be inflated without pushing the guest system to swap: %llukB\n", usable);
+		printf("Memory usage: %f\n", ((float)unused/(float)available));
 	}
 }
 
@@ -57,8 +97,7 @@ int main(int argc, char * argv) {
       for(int i = 0; i < numOfDomains; i++) virDomainSetMemoryStatsPeriod(allDomains[i], 1, 0);
       sleep(2);
       printMemoryStats();
-      // coordinate memory here.
-
+      balanceMemory();
       freeAllDomainPointers();
       virConnectClose(hypervisor);
       sleep(5);	
